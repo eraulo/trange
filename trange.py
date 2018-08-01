48788a97a4824eb5514a87a5891c025c099fa03e
eraulo <dedeus_mariano@yahoo.com>
2018-08-01 11:04:47 +0900

    add email

--- trange.py
+++ trange.py
@@ -1,47 +1,48 @@
 #!/usr/bin/env python
 
-__author__ = "Mariano de Deus dedeus_mariano@yahoo.com"
+__author__ = "Mariano de Deus"
+__email__ = "dedeus_mariano@yahoo.com"
 
 def Range(first=0, number=0, step=0 ):
     if (first or number or step) and number:
         if number:
-            rlist = [first]
+            tlist = [first]
             while True:
-                if len(rlist) ==  1:
-                    if rlist[0] == 0:
-                        rlist += [len(rlist)]
+                if len(tlist) ==  1:
+                    if tlist[0] == 0:
+                        tlist += [len(tlist)]
                     else:
-                        rlist += [abs(~len(rlist))]
-                elif rlist[-1] < number:
-                    rlist += [abs(~rlist[-1])]
-                elif rlist[-1] == number:
+                        tlist += [abs(~len(tlist))]
+                elif tlist[-1] < number:
+                    tlist += [abs(~tlist[-1])]
+                elif tlist[-1] == number:
                     break
             if first != 0 and step != 0:
-                for n in rlist[(first-1):(number-1):step]:
+                for n in tlist[(first-1):(number-1):step]:
                     yield n
             elif first == 0 and step != 0:
-                for n in rlist[first:number:step]:
+                for n in tlist[first:number:step]:
                     yield n
             elif first != 0 and step == 0:
-                for n in rlist[(first-1):(number-1)]:
+                for n in tlist[(first-1):(number-1)]:
                     yield n
             elif first != 0:
-                for n in rlist[(first-1):(number-1)]:
+                for n in tlist[(first-1):(number-1)]:
                     yield n
             else:
-                for n in rlist[first:number]:
+                for n in tlist[first:number]:
                     yield n
     elif first != 0:
         number = first
-        rlist = [0]
+        tlist = [0]
         while True:
-            if len(rlist) ==  1:
-                rlist += [len(rlist)]
-            elif rlist[-1] < number:
-                rlist += [abs(~rlist[-1])]
-            elif rlist[-1] == number:
+            if len(tlist) ==  1:
+                tlist += [len(tlist)]
+            elif tlist[-1] < number:
+                tlist += [abs(~tlist[-1])]
+            elif tlist[-1] == number:
                     break
-        for n in rlist[0:number]:
+        for n in tlist[0:number]:
             yield n
     else:
         return
